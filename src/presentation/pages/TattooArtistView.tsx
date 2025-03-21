d399de86f528c2b77ac439cbf823577c659bb786
luanaFOliveira <luanaFOliveirabr@gmail.com>
2025-03-21 17:05:54 -0300

    feat: added need to be authenticated to request a quote

--- src/presentation/pages/TattooArtistView.tsx
+++ src/presentation/pages/TattooArtistView.tsx
@@ -1,13 +1,19 @@
 'use client';
 import React, { useEffect, useState } from "react";
-import { CircularProgress, Typography, Card, CardContent, CardMedia, Box, Grid } from "@mui/material";
+import { CircularProgress, Typography, Card, CardContent, CardMedia, Box, Grid, Button } from "@mui/material";
 import { TattooArtist } from "@/domain/entities/tattoo-artist";
 import { TattooArtistApi } from "@/infra/api/tattooArtistApi";
 import QuoteModal from "@/presentation/components/QuoteModal";
+import { useAuth } from "@/presentation/context/AuthContext";
+import { useRouter } from 'next/navigation'
+import { toast } from "react-toastify";
 
 export default function TattooArtistView({ tattooArtistId }: { tattooArtistId: string }) {
   const [artist, setArtist] = useState<TattooArtist | null>(null);
   const [loading, setLoading] = useState(true);
+  const [openModal, setOpenModal] = useState(false);
+  const { user, isAuthenticated } = useAuth(); 
+  const router = useRouter();
 
   useEffect(() => {
     const fetchArtist = async () => {
@@ -17,6 +23,7 @@ export default function TattooArtistView({ tattooArtistId }: { tattooArtistId: s
         setArtist(data);
       } catch (error) {
         console.error("Erro ao buscar tatuador:", error);
+        toast.error("Erro ao buscar tatuador");
       } finally {
         setLoading(false);
       }
@@ -33,13 +40,23 @@ export default function TattooArtistView({ tattooArtistId }: { tattooArtistId: s
     return <Typography variant="h6" color="error">Tatuador não encontrado</Typography>;
   }
 
+
+  const handleQuoteRequest = () => {
+    if (!isAuthenticated) {
+      toast.error("Para solicitar um orçamento é necessário estar logado");
+      router.push("/login");
+    } else {
+      setOpenModal(true);
+    }
+  };
+
   return (
     <Box sx={{ maxWidth: "1000px", mx: "auto", mt: 5 }}>
       <Card sx={{ display: "flex", border: "2px solid #6A0DAD", borderRadius: 3, boxShadow: 3 }}>
         <CardMedia
           component="img"
           image="/placeholder.jpg"
-          // image={artist.imageUrl || "/placeholder.jpg"} // Usa a imagem do artista ou um placeholder
+          // image={artist.imageUrl || "/placeholder.jpg"} 
           alt={artist.name}
           sx={{ width: 300, height: "100%", objectFit: "cover", borderRadius: "5px 0 0 5px" }}
         />
@@ -62,7 +79,10 @@ export default function TattooArtistView({ tattooArtistId }: { tattooArtistId: s
           </Box>
           
         </CardContent>
-        <QuoteModal tattooArtist={artist}/>
+        <Button variant="contained" color="primary" onClick={handleQuoteRequest}>
+          Solicitar Orçamento
+        </Button>
+        {openModal && <QuoteModal tattooArtist={artist} />}
       </Card>
       <Typography variant="h5" sx={{ mt: 4, mb: 2, textAlign: "center", fontWeight: "bold" }}>
         Portfólio
